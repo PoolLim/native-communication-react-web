8c1a4d208a1a1920e2ae678a8cd9351e3716f598
bolt-Pool <pool@bolt.finance>
2022-03-17 12:26:12 +0900

    call native function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const callNative = () => {
+    window.webkit.messageHandlers.callbackHandler.postMessage(
+      "callNativeFunction"
+    );
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button
+          onClick={callNative}
+          style={{
+            width: 200,
+            height: 130,
+            fontSize: 30,
+          }}
         >
-          Learn React
-        </a>
+          Call Native
+        </button>
       </header>
     </div>
   );
